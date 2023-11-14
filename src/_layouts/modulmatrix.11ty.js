9f9573519ce6a7cda40259ed674bf09ac22d806b
Matthias Böhmer <matboehmer@googlemail.com>
2023-11-14 13:53:53 +0100

    Modulmatrix gebaut

---
+++ src/_layouts/modulmatrix.11ty.js
@@ -0,0 +1,167 @@
+module.exports = {
+	data: {
+		layout: "default.11ty.js",
+		bodyClass: "curriculum",
+	},
+	render(data) {
+    
+    eleventy: this;
+
+    const { collection } = data;
+    // const curriculumTools = require('./components/curriculumTools.11ty.js');
+    moduls = data.collections[collection];
+
+    handlungsfelder = data.collections.handlungsfelder;
+    // const curriculumList = curriculumTools.getCurriculumList({
+    //   moduls: data.collections[collection],
+    //   terms: data.terms,
+    //   maxCPS: data.maxCPS,
+    //   studienverlauf: data.studienverlauf,
+    //   data,
+    //   eleventy: this
+    // });
+
+    // const curriculumVerlaufTable = curriculumTools.getCurriculumVerlaufsplanTable({
+    //   moduls: data.collections[collection],
+    //   terms: data.terms,
+    //   groups: data.groups,
+    //   maxCPS: data.maxCPS,
+    //   studienverlauf: data.studienverlauf,
+    //   data,
+    //   eleventy: this
+    // });
+
+    // const allModuls = curriculumTools.getAllModuls({
+    //   moduls: data.collections[collection],
+    //   data,
+    //   eleventy: this
+    // });
+
+    // const curriculumVerlaufList = data.showModuleList && data.showModuleList === true
+    //   ? `<section>${curriculumList}</section>` : '';
+
+
+    const modulRows = moduls.map((modulItem) => {
+      const modul = modulItem.data;
+
+      return `
+      <tr>
+                <!-- Modul -->
+                <td><a href="${modulItem.url}">${modul.title}</a></td>
+                <td>todo</td>
+                <td>${modul.kreditpunkte}</td>
+                <th>${(modul.angebotImWs && modul.angebotImSs) ? "immer" : ""}${(modul.angebotImWs && !modul.angebotImSs) ? "WiSe" : ""}${(!modul.angebotImWs && modul.angebotImSs) ? "SoSe" : ""}</th>
+
+                <!-- Handlungsfelder -->
+                <th>${modul.handlungsfelder?.DUX ? "DUX" : ""}</th>
+                <th>${modul.handlungsfelder?.DEV ? "DEV" : ""}</th>
+                <th>${modul.handlungsfelder?.EXA ? "EXA" : ""}</th>
+                <th>${modul.handlungsfelder?.CREA ? "CREA" : ""}</th>
+                <th>${modul.handlungsfelder?.INDI ? "INDI" : ""}</th>
+
+                <!-- Zuordnung Kompetenzen -->
+                <th>${modul.kompetenzen?.DUX.anforderungen_bedarfe ? "x" : ""}</th>
+                <th>${modul.kompetenzen?.DUX.konzepte ? "x" : ""}</th>
+                <th>${modul.kompetenzen?.DUX.gestaltung ? "x" : ""}</th>
+              
+                <th>${modul.kompetenzen?.DEV.technologie ? "x" : ""}</th>
+                <th>${modul.kompetenzen?.DEV.entwurf ? "x" : ""}</th>
+                <th>${modul.kompetenzen?.DEV.implementierung ? "x" : ""}</th>
+              
+                <th>${modul.kompetenzen?.EXA.medien ? "x" : ""}</th>
+                <th>${modul.kompetenzen?.EXA.exploration_kreativitaet ? "x" : ""}</th>
+                <th>${modul.kompetenzen?.EXA.prototyping ? "x" : ""}</th>
+              
+                <th>${modul.kompetenzen?.CREA.innovation ? "x" : ""}</th>
+                <th>${modul.kompetenzen?.CREA.management ? "x" : ""}</th>
+                <th>${modul.kompetenzen?.CREA.kommunikation ? "x" : ""}</th>
+              
+                <th>${modul.kompetenzen?.INDI.analyse_studien_experimente ? "x" : ""}</th>
+                <th>${modul.kompetenzen?.INDI.situated_interaction ? "x" : ""}</th>
+                <th>${modul.kompetenzen?.INDI.ethik_recht ? "x" : ""}</th>
+                <th>${modul.kompetenzen?.INDI.selbstlernen ? "x" : ""}</th>
+
+                <!-- Zuordnung Studiengangkriterien -->
+                <th>${modul.studiengangkriterien?.globalcitizenship ? "x" : ""}</th>
+                <th>${modul.studiengangkriterien?.internationalisierung ? "x" : ""}</th>
+                <th>${modul.studiengangkriterien?.interdisziplinaritaet ? "x" : ""}</th>
+                <th>${modul.studiengangkriterien?.transfer ? "x" : ""}</th>
+
+                <!-- Prüfungen -->
+                <th>${modul.studienleistungen?.Einzelleistung ? "1" : "?"}</th>
+              </tr>
+        `
+
+    });
+
+
+
+    kompetenzSpalten = handlungsfelder.map(hf => {
+      console.log("=======================================================================================");
+      //console.log(hf.data.competencies);
+      return hf.data.competencies.map( cmpt => {
+        return `<th>${cmpt.title}</th>\n` 
+      }).join();
+    }).join();
+
+		return `
+			<main>
+				<section class="module-core-data">
+					<header>
+						<h1>${data.title}</h1>
+					</header>
+				</section>
+
+        <section class="content">
+          ${data.content}
+        </section>
+
+        <section class="has-seperator">
+        <h2>Modulmatrix</h2>
+          <table class="table-curriculum is-striped is-narrow">
+          <thead>
+          <tr>
+            <th colspan=4>Modul</th>
+            <th colspan=5>Handlungsfelder</th>
+            <th colspan=16>Zuordnung Kompetenzen</th>
+            <th colspan=4>Zuordnung Studiengangkriterien</th>
+            <th rowspan=2>Prüfungen</th>
+          </tr>
+            <tr>
+              <!-- Modul -->
+              <td>Modul</td>
+              <td>Pflicht/Wahl</td>
+              <td>ECTS</td>
+              <th>Semester</td>
+
+              <!-- Handlungsfelder -->
+              <th>DUX</th>
+              <th>DEV</th>
+              <th>EXA</th>
+              <th>CREA</th>
+              <th>INDI</th>
+
+              <!-- Zuordnung Kompetenzen -->
+              ${kompetenzSpalten}
+
+              <!-- Zuordnung Studiengangkriterien -->
+              <th>Global Citizenship</th>
+              <th>Internationalisierung</th>
+              <th>Interdisziplinarität</th>
+              <th>Transfer</th>
+
+            </tr>
+            </thead>
+            <tbody>
+            ${modulRows}
+              
+            </tbody> 
+
+          </table>
+        </section>
+
+
+			</main>
+		`;
+	}
+}
--- src/modulbeschreibungen-master-mpo5/MA_EXA_Ubiquitous-Computing.md
+++ src/modulbeschreibungen-master-mpo5/MA_EXA_Ubiquitous-Computing.md
@@ -23,6 +23,43 @@ meta:
   status: rfreview    
   authors: Matthias
   reviewers: Matthias
+
+# zuordnung Kompetenzorientierung und Studiengangskriterien für Modulmatrix
+handlungsfelder:
+  DUX: 0
+  DEV: 1
+  EXA: 1
+  CREA: 1
+  INDI: 1
+
+kompetenzen:
+  DUX:
+    anforderungen_bedarfe: 1
+    konzepte: 0
+    gestaltung: 1
+  DEV:
+    technologie: 0
+    entwurf: 0
+    implementierung: 1
+  EXA:
+    medien: 1
+    exploration_kreativitaet: 1
+    prototyping: 1
+  CREA:
+    innovation: 1
+    management: 0
+    kommunikation: 0
+  INDI:    
+    analyse_studien_experimente: 0
+    situated_interaction: 1
+    ethik_recht: 0
+    selbstlernen: 0
+
+studiengangkriterien:
+  globalcitizenship: 0
+  internationalisierung: 0
+  interdisziplinaritaet: 0
+  transfer: 1
 ---
 
 ## Kurzbeschreibung
